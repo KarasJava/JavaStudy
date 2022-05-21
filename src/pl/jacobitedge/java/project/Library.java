bfba6d1b94e4651ddf56570f62feb97699ac0f53
jacobitedge <wojciechkaras14@gmail.com>
2022-05-21 19:04:55 +0200

    release library v0.1 app

--- src/pl/jacobitedge/java/project/Library.java
+++ src/pl/jacobitedge/java/project/Library.java
@@ -1,5 +1,21 @@
 package pl.jacobitedge.java.project;
 
 public class Library {
+    public static void main(String[] args) {
+
+        final String appName = "Library v0.1";
+
+        String title = "Harry Potter";
+        String author = "J.K. Rowling";
+        int releaseDate = 1997;
+        int pages = 296;
+        String publisher = "Pottermore";
+        String isbn = "978-83-8008-211-3.";
+
+        System.out.println(" Welcome to app:" + appName);
+
+        System.out.println(title + " " + author + " " + releaseDate + " " + pages + " " +publisher + " " +isbn);
+
+    }
 
 }
