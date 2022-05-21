c28a07d142ee1634ea5243732dbbf5f295510523
jacobitedge <wojciechkaras14@gmail.com>
2022-05-21 16:31:11 +0200

    adding DataType.java and notes

---
+++ src/pl/jacobitedge/java/typDanych/DataType.java
@@ -0,0 +1,18 @@
+package pl.jacobitedge.java.typDanych;
+
+public class DataType {
+    public static void main(String[] args) {
+        System.out.println(199999999L);
+        //UTF16 unicode character table
+        char sign = '\u2602';
+        System.out.println(sign);
+
+        //print something in new line by type \n in quotation marks ("text \n")
+        System.out.print("Print something in new line \n");
+
+        //analogy to previous line when we use \t it display tabulator
+
+        System.out.print("Print something in new line \t text text text");
+
+    }
+}
