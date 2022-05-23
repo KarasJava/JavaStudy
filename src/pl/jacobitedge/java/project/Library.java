bc1521c1a964ecd5c640ccac797eaf82740e3c2e
jacobitedge <wojciechkaras14@gmail.com>
2022-05-23 19:40:55 +0200

    library v0.2 create Book class with fields, modify main Library.class

--- src/pl/jacobitedge/java/project/Library.java
+++ src/pl/jacobitedge/java/project/Library.java
@@ -2,20 +2,24 @@ package pl.jacobitedge.java.project;
 
 public class Library {
     public static void main(String[] args) {
+        final String appName = "Biblioteka v0.2";
 
-        final String appName = "Library v0.1";
+        Book book1 = new Book();
+        book1.title = "W pustyni i w puszczy";
+        book1.author = "Henryk Sienkiewicz";
+        book1.releaseDate = 2010;
+        book1.pages = 296;
+        book1.publisher = "Greg";
+        book1.isbn = "9788373271890";
 
-        String title = "Harry Potter";
-        String author = "J.K. Rowling";
-        int releaseDate = 1997;
-        int pages = 296;
-        String publisher = "Pottermore";
-        String isbn = "978-83-8008-211-3.";
-
-        System.out.println(" Welcome to app:" + appName);
-
-        System.out.println(title + " " + author + " " + releaseDate + " " + pages + " " +publisher + " " +isbn);
-
+        System.out.println(appName);
+        System.out.println("Książki dostępne w bibliotece:");
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.releaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
     }
 
 }
