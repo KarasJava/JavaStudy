1391a080f4f9664b8d5ba35c84fc0810e88922fa
jacobitedge <wojciechkaras14@gmail.com>
2022-05-24 20:10:53 +0200

    methods homework

---
+++ src/pl/jacobitedge/java/metodyIKontruktory/homework/methods/Calculator.java
@@ -0,0 +1,16 @@
+package pl.jacobitedge.java.metodyIKontruktory.homework.methods;
+
+public class Calculator {
+    void add(double a, double b) {
+        System.out.println(a+b);
+    }
+    void subtract(double a, double b) {
+        System.out.println(a-b);
+    }
+    void multiply(double a, double b) {
+        System.out.println(a*b);
+    }
+    void divide(double a, double b) {
+        System.out.println(a/b);
+    }
+}
---
+++ src/pl/jacobitedge/java/metodyIKontruktory/homework/methods/CalculatorTest.java
@@ -0,0 +1,13 @@
+package pl.jacobitedge.java.metodyIKontruktory.homework.methods;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.add(1,5);
+        calculator.subtract(1,5);
+        calculator.divide(1,5);
+        calculator.multiply(1,5);
+
+    }
+
+}
